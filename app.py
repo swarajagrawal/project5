1c6b97205d013260fe5c70138990df37fe078ea1
Swaraj Agrawal <Swarajagrawal@yahoo.com>
2023-04-05 19:24:55 +0530

    lint fail 1

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def home():
     return "Hello World, I am a Udacity student"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=Truee)
