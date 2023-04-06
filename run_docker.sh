b4e6acd4b7c0b087d3a40048f9f1f7dcd80e0e40
Swaraj Agrawal <Swarajagrawal@yahoo.com>
2023-04-06 10:02:37 +0530

    docker run4

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+## Complete the following steps to get Docker running local
 
 # Build image and add a descriptive tag
 docker build --tag=capstoneproject .
